9fe4ca4f7151e9938c71230bacd4a88538844c49
Jonny Buchanan <jonathan.buchanan@gmail.com>
2015-11-15 07:56:25 +0000

    Update tests and replace t.end() with t.plan()

--- test/GridForms-test.js
+++ test/GridForms-test.js
@@ -6,21 +6,27 @@ import {renderIntoDocument as render} from 'react-addons-test-utils'
 import {GridForm, Fieldset, Row, Field} from 'src/index'
 
 test('GridForm', t => {
-  t.plan(4)
+  t.plan(6)
 
   let node = render(<GridForm action="/test"/>)
   let el = findDOMNode(node)
   t.equal(el.tagName.toLowerCase(), 'form', 'renders a <form> by default')
-  t.equal(el.className, 'grid-form', 'gives its element a grid-form class')
+  t.equal(el.className, 'grid-form', 'gives its element a grid-form class by default')
   t.equal(el.getAttribute('action'), '/test', 'passes other props when rendering')
 
-  node = render(<GridForm component="div"/>)
+  node = render(<GridForm className="special" component="div"/>)
   el = findDOMNode(node)
-  t.equal(el.tagName.toLowerCase(), 'div', 'rendered component can be customised')
-  t.end()
+  t.equal(el.tagName.toLowerCase(), 'div', 'uses a cdifferent component when given')
+  t.equal(el.className, 'grid-form special', 'adds an additional className when given')
+
+  node = render(<GridForm custom className="my-grid-form"/>)
+  el = findDOMNode(node)
+  t.equal(el.className, 'my-grid-form', 'drops grid-form class when custom flag is given')
 })
 
 test('Fieldset', t => {
+  t.plan(4)
+
   let node = render(<Fieldset legend="Legendary" name="test"/>)
   let el = findDOMNode(node)
   let legend = el.querySelector('legend')
@@ -30,11 +36,12 @@ test('Fieldset', t => {
 
   node = render(<Fieldset/>)
   el = findDOMNode(node)
-  t.ok(!el.querySelector('legend'), "doesn't render a <legend> when a legend prop isn't given")
-  t.end()
+  t.ok(!el.querySelector('legend'), 'only renders a legend> when a legend prop is given')
 })
 
 test('Row ', t => {
+  t.plan(2)
+
   let node = render(<Row className="test">
     <Field/>
     <Field span="2"/>
@@ -43,10 +50,11 @@ test('Row ', t => {
   t.equal(findDOMNode(node).getAttribute('data-row-span'), '6',
           'sums the spans of its children and sets the total as data-row-span')
   t.equal(findDOMNode(node).className, 'test', 'passes other props when rendering')
-  t.end()
 })
 
 test('Field ', t => {
+  t.plan(4)
+
   let node = render(<Field className="test"/>)
   t.equal(findDOMNode(node).getAttribute('data-field-span'), '1', 'defaults data-field-span to 1')
   t.equal(findDOMNode(node).className, 'test', 'passes other props when rendering')
@@ -54,5 +62,4 @@ test('Field ', t => {
   t.equal(findDOMNode(node).getAttribute('data-field-span'), '2', 'takes span as a string')
   node = render(<Field span={2}/>)
   t.equal(findDOMNode(node).getAttribute('data-field-span'), '2', 'takes span as a number')
-  t.end()
 })
